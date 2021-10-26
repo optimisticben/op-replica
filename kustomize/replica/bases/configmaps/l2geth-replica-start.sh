1e6d4e6d2941910ef22dfa91351c5ccd121dc052
Ben Wilson <bwilson@optimism.io>
2021-10-26 10:56:36 -0400

    Fixed l2geth template
    Updated kovan envs
    
    Updated docker-compose
    
    Updated ROLLUP_MIN_L2_GAS_LIMIT
    
    Cleaning up scripts
    
    Split envs for k8s and docker-compose reuse
    
    Mainnet gen5 staging 1 setup

---
+++ kustomize/replica/bases/configmaps/l2geth-replica-start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eou
+
+if [[ -z $DATADIR ]]; then
+    echo "Must pass DATADIR"
+    exit 1
+fi
+if [[ -z $BLOCK_SIGNER_ADDRESS ]]; then
+    echo "Must pass BLOCK_SIGNER_ADDRESS"
+    exit 1
+fi
+
+geth \
+  --verbosity=5 \
+  --datadir=$DATADIR \
+  --password=$DATADIR/password \
+  --allow-insecure-unlock \
+  --unlock=$BLOCK_SIGNER_ADDRESS \
+  --mine \
+  --miner.etherbase=$BLOCK_SIGNER_ADDRESS
